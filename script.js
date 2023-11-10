ce699b07f42863ef5ece8b322bec9468c88acc3a
Irene Efimova <142536445+iki-welt@users.noreply.github.com>
2023-11-10 13:38:54 +0300

    Добавлет скрипт

---
+++ script.js
@@ -0,0 +1,5 @@
+let modalOpener = document.querySelector('.btn-modal');
+let modal = do.querySelector('.modal-container');
+modalOpener.onclick = function() {
+modal.classList.add('modal-active')
+};
