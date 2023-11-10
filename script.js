ac7e2ffc0c2fdb871e182654a3e2db4663ba37e5
Irene Efimova <142536445+iki-welt@users.noreply.github.com>
2023-11-10 16:11:32 +0300

    Добавляет скрипт и форматирование кода
    Скрипт для открытия/закрытия модального окна, для кнопок +/- в модальном окне

--- script.js
+++
@@ -1,5 +0,0 @@
-let modalOpener = document.querySelector('.btn-modal');
-let modal = do.querySelector('.modal-container');
-modalOpener.onclick = function() {
-modal.classList.add('modal-active')
-};
